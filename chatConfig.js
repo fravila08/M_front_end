c2dfa234e72e1efe452558c80629181e62d91777
DrAcula27 <danielle@kendallspace.net>
2023-07-17 17:07:27 -0600

    set up chat config and client

---
+++ chatConfig.js
@@ -0,0 +1,14 @@
+export const chatApiKey = 'vnfrejsn6scx';
+
+// ! when integrating, fetch user from backend
+// * only have one of these users uncommented at a time
+
+// export const chatUserId = "ancient-hall-3";
+// export const chatUserToken =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYW5jaWVudC1oYWxsLTMiLCJleHAiOjE2ODkyNzg1OTd9.G7xdg8EmT5PvGzKnjyGXCnxRfoxkGzgCXtXKlBYqelo";
+// export const chatUserName = "ancient";
+
+export const chatUserId = 'billowing-art-5';
+export const chatUserToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYmlsbG93aW5nLWFydC01IiwiZXhwIjoxNjg5Mjg1OTU4fQ.IMtaZAIWbM7e4mYrcSjOaMtuMSdHnJ1BrBBtO2j_XwA';
+export const chatUserName = 'billowing';
